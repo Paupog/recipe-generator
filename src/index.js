ed508e338f30e722a35d37f052065701a702a51c
Paupog <154589264+Paupog@users.noreply.github.com>
2024-11-08 00:16:56 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ function generateRecipe(event){
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "dfd0t6478f0367aa31fdf9o1c7ab1790";    
     let context = 
-    "You are an experienced chef in a cafe who loves to cook quick and tasty vegetarian food. Your mission is to generate a recipe that is easy to follow, and include maximum 5 ingredients. Separate the recipe in a readable way. I want ingredients to be in bullet points in basic HTML and separate each line with a <br /> above instructions. Make sure to follow the user instructions. Sign the recipe with 'Paulina AI' inside a <strong> element at the end of the recipe and NOT at the beginning. Dont use #";
+    "You are an experienced chef in a cafe who loves to cook quick and tasty vegetarian food. Your mission is to generate a recipe that is easy to follow, and include maximum 5 ingredients. Separate the recipe in a readable way. I want ingredients to be in bullet points in basic HTML and separate each line with a <br /> above instructions. Make sure to follow the user instructions. Sign the recipe with 'Paulina AI' inside a <strong> element at the end of the recipe and NOT at the beginning. Dont use hashtags";
     let prompt = `User instructions: Generate a recipe out of these ingredients ${instructionsInput.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`; 
     
